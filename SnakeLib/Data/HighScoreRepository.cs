3c90761973a24ef0a76ffbdef8f8ae299b099094
brandonchastain <brandonchastain@gmail.com>
2023-12-26 04:33:23 +0000

    remove files (not supported by wasm)

--- SnakeLib/Data/HighScoreRepository.cs
+++ SnakeLib/Data/HighScoreRepository.cs
@@ -10,46 +10,21 @@ namespace SnakeLib.Data
     public class HighScoreRepository : IHighScoreRepository
     {
         public static HighScoreRepository Instance = new HighScoreRepository();
-        private static readonly string FilePath = @"%HOME%\highscores.csv";
 
         private HighScoreRepository()
         {
-            if (!File.Exists(FilePath))
-            {
-                File.Create(FilePath);
-                File.WriteAllLines(FilePath, new[] { "BCC,200" });
-            }
         }
+        
         public IEnumerable<HighScore> GetHighScores()
         {
-            var scores = new List<HighScore>();
-            string[] contents = File.ReadAllLines(FilePath);
-
-            foreach (string line in contents)
-            {
-                string[] parts = line.Split(",");
-                string username = parts[0];
-                int score = int.Parse(parts[1]);
-
-                scores.Add(new HighScore()
-                {
-                    Username = username,
-                    Score = score,
-                });
-            }
-
-            return scores;
+            var a = new HighScore();
+            a.Username = "abc";
+            a.Score = 200;
+            return new List<HighScore>(){a};
         }
 
         public void SaveHighScores(IEnumerable<HighScore> scores)
         {
-            var sb = new StringBuilder();
-            foreach (var score in scores)
-            {
-                sb.Append($"{score.Username},{score.Score}\n");
-            }
-
-            File.WriteAllText(FilePath, sb.ToString());
         }
     }
 }
