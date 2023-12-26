537de911bf59d1b4a5fea1b130835185de7e0f2c
brandonchastain <brandonchastain@gmail.com>
2023-12-26 07:06:44 +0000

    high score web api

---
+++ DataWebApi/Controllers/HighScoreController.cs
@@ -0,0 +1,45 @@
+using Azure;
+using Azure.Data.Tables;
+using Microsoft.AspNetCore.Mvc;
+using SnakeLib.Contracts;
+
+namespace DataWebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HighScoreController : ControllerBase
+{
+    private const string StorageUri = "https://snakescores.table.core.windows.net/";
+    private const string AccountName = "snakescores";
+    private const string StorageAccountKey = Environment.GetEnvironmentVariable("BsnakeStorageAccountKey");
+    
+    private readonly ILogger<HighScoreController> _logger;
+    private readonly TableServiceClient serviceClient;
+    private readonly TableClient tableClient;
+
+    public HighScoreController(ILogger<HighScoreController> logger)
+    {
+        _logger = logger;
+        // Construct a new "TableServiceClient using a TableSharedKeyCredential.
+
+        serviceClient = new TableServiceClient(
+            new Uri(StorageUri),
+            new TableSharedKeyCredential(AccountName, StorageAccountKey));
+
+        tableClient = serviceClient.GetTableClient("HighScores");
+    }
+
+    [HttpGet(Name = "GetHighScores")]
+    public IEnumerable<HighScore> Get()
+    {
+        Pageable<HighScore> queryResultsFilter = tableClient.Query<HighScore>();
+        var res = new List<HighScore>();
+        // Iterate the <see cref="Pageable"> to access all queried entities.
+        foreach (HighScore score in queryResultsFilter)
+        {
+            res.Add(score);
+        }
+
+        return res;
+    }
+}
--- SnakeLib/Contracts/HighScore.cs
+++ SnakeLib/Contracts/HighScore.cs
@@ -1,9 +1,16 @@
 namespace SnakeLib.Contracts
 {
+    using System;
+    using Azure;
+    using Azure.Data.Tables;
 
-    public class HighScore
+    public class HighScore : ITableEntity
     {
         public string Username { get; set; }
         public int Score { get; set; }
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
     }
 }
